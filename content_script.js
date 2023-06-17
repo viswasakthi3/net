7f5f8ed5c0e55b937bcf8632040b883557ed4954
vishwa <viswasakthi2@gmail.com>
2023-06-17 18:56:48 +0530

    .

--- content_script.js
+++ content_script.js
@@ -1,43 +1,66 @@
-if (window.location.hostname === "www.netflix.com") {
-  if (
-    window.netflix &&
-    window.netflix.reactContext &&
-    window.netflix.reactContext.models &&
-    window.netflix.reactContext.models.userInfo &&
-    window.netflix.reactContext.models.userInfo.data
-  ) {
-    const shaktiApiToken =
-      window.netflix.reactContext.models.userInfo.data.authURL;
-
-    if (shaktiApiToken) {
-      console.log("Shakti API token:", shaktiApiToken);
-      const viewingActivityUrl = `https://www.netflix.com/api/shakti/${shaktiApiToken}/viewingactivity`;
-
-      fetch(viewingActivityUrl)
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
+// Add the getShakti function
+function getShakti(onSucceed, onError, opt) {
+  const viewingActivityUrl = "https://www.netflix.com/WiViewingActivity";
+  $.ajax({
+    url: viewingActivityUrl,
+    timeout: 15000,
+    success: (data) => {
+      if (
+        data &&
+        (data.indexOf("https://www.netflix.com/Login?nextpage=") > -1 ||
+          data.indexOf("login-submit-button") > -1)
+      ) {
+        onError("login");
+      } else {
+        let sPos2 = data.indexOf('BUILD_IDENTIFIER":"') + 19;
+        if (sPos2 > 25) {
+          let _sh2 = data
+            .substr(sPos2, data.indexOf('"', sPos2) - sPos2)
+            .replace(/["\s]/g, "");
+          if (_sh2.match(/[A-Za-z0-9]+/g)) {
+            const shaktiApiToken = _sh2;
+            onSucceed(shaktiApiToken, opt);
           } else {
-            throw new Error(`Request failed with status ${response.status}`);
+            onError("symbols");
           }
-        })
-        .then((data) => {
-          console.log("Viewing activity data:", data);
-          // Use chrome.extension.sendMessage instead of chrome.runtime.sendMessage
-          chrome.extension.sendMessage({
-            message: "viewing_activity_data",
-            data: data,
-          });
-        })
-        .catch((error) => {
-          console.error("Error fetching viewing activity:", error);
-        });
-    } else {
-      console.error("Shakti API token not found");
-    }
-  } else {
-    console.error("Required object not found in the JavaScript environment");
-  }
+        } else {
+          onError("position");
+        }
+      }
+    },
+    error: function (err) {
+      onError("failed to get");
+    },
+  });
+}
+
+// Modify this function to use the shaktiApiToken obtained from getShakti
+function fetchViewingActivity(shaktiApiToken, opt) {
+  const viewingActivityUrl = `https://www.netflix.com/api/shakti/${shaktiApiToken}/viewingactivity`;
+  fetch(viewingActivityUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+    })
+    .then((data) => {
+      console.log("Viewing activity data:", data);
+      chrome.extension.sendMessage({
+        message: "viewing_activity_data",
+        data: data,
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching viewing activity:", error);
+    });
+}
+
+if (window.location.hostname === "www.netflix.com") {
+  getShakti(fetchViewingActivity, (error) => {
+    console.error("Error obtaining Shakti API token:", error);
+  });
 } else {
   console.error("This extension only works on Netflix's website");
 }
