0d2559a2a65f4ec03ca24fc297329d5d5d56fe8d
vishwa <viswasakthi2@gmail.com>
2023-06-17 20:13:40 +0530

    z

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
-document.getElementById("fetchData").addEventListener("click", () => {
+document.getElementById("button").addEventListener("click", async () => {
+  // Get the active tab
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  const activeTab = tabs[0];
+
+  // Execute the script on the active tab
   chrome.scripting.executeScript({
-    target: { tabId: chrome.tabs.TAB_ID_CURRENT },
+    target: { tabId: activeTab.id }, // Provide the tabId here
     files: ["content.js"],
   });
 });
